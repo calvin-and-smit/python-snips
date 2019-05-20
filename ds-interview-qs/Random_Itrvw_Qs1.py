9df2bfbbc4856ae4dd002995090328723de9005d
Smit Mehta <mehta.smit92@gmail.com>
2019-05-20 14:11:42 -0400

    Delete Random_Itrvw_Qs1.py

--- ds-interview-qs/Random_Itrvw_Qs1.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a program that squares an integer and prints the result.
-Input:Your program should read lines from standard input. 
-Each line will contain a positive integer.Output:For each line of input, print to 
-standard output the square of the number. Print out each result on a new line.
-"""
-import sys
-for line in sys.stdin:
-    print(int(line)**2)
