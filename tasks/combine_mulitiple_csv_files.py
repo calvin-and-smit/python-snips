3ea2977ac1ac5a1f21915a69acc4387edcc3bfc3
Smit Mehta <mehta.smit92@gmail.com>
2019-08-20 17:00:28 -0400

    Update combine_mulitiple_csv_files.py

--- tasks/combine_mulitiple_csv_files.py
+++ tasks/combine_mulitiple_csv_files.py
@@ -1,14 +1,17 @@
 
-
+#Importing packages
 import pandas as pd
 import glob
 import os
 
+#Change working directory
+os.chdir('mydir/data')
 
-os.chdir('/Users/smehta/Desktop/hps/data')
-
+#Generate list of csv files in the folder
 all_csv_files = [f for f in glob.glob('*.csv')]
 
+#Concatenate all the csv files
 csv_union = pd.concat([pd.read_csv(f) for f in all_csv_files])
 
+#Export the final csv
 csv_union.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
